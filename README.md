03e30e3954fb5243a1908b8a1d4fede53a53c9be
RiteshBirajdar <105624241+RiteshBirajdar@users.noreply.github.com>
2023-08-12 13:22:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Practice
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-yggqqu)
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+@Component({
+  standalone:true,
+  selector: 'app-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports:[HeroesComponent,HeroDetailComponent]
+})
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
